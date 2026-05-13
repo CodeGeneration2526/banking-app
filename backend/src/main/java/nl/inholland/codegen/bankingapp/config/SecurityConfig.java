badafe1322bc7912340a9a807eba83f9ef6a98ff
Md Asif Iqbal <117485071+Asif135-hub@users.noreply.github.com>
2026-05-14 01:28:54 +0200

    H2 console and comments removed

--- backend/src/main/java/nl/inholland/codegen/bankingapp/config/SecurityConfig.java
+++ backend/src/main/java/nl/inholland/codegen/bankingapp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,13 +16,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private static final String[] PUBLIC_ENDPOINTS = {
-       "/",
+        "/",
         "/auth/**",
         "/swagger-ui.html",
         "/swagger-ui/**",
-        "/v3/api-docs/**",
-        "/h2-console/**"
+        "/v3/api-docs/**"
     };
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/h2-console/**");
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,8 +38,8 @@ public class SecurityConfig {
                 .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
                 .anyRequest().authenticated()
             )
-            .formLogin(AbstractHttpConfigurer::disable)   
-            .httpBasic(AbstractHttpConfigurer::disable); 
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
