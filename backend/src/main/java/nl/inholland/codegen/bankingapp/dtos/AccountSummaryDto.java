23c08e9e4a6310c89f88ddd49b26fe3b2c120055
piguycs <piguycs@pm.me>
2026-05-14 08:46:05 +0200

    removing all the DTOs which had no references

--- backend/src/main/java/nl/inholland/codegen/bankingapp/dtos/AccountSummaryDto.java
+++
@@ -1,8 +0,0 @@
-package nl.inholland.codegen.bankingapp.dtos;
-
-public record AccountSummaryDto(
-    long accountId,
-    String iban,
-    String accountType,
-    int balanceCents
-) {}
--- backend/src/main/java/nl/inholland/codegen/bankingapp/dtos/LoginResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package nl.inholland.codegen.bankingapp.dtos;
-
-public record LoginResponseDTO(
-    String token,
-    String role
-) {}
