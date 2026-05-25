9f443452e055ad241619ddc68205b60c1d8ffbdf
piguycs <piguycs@pm.me>
2026-05-25 22:39:36 +0200

    updating the account model to have defaults and better constraints

--- backend/src/main/java/nl/inholland/codegen/bankingapp/models/Account.java
+++ backend/src/main/java/nl/inholland/codegen/bankingapp/models/Account.java
@@ -1,7 +1,7 @@
 package nl.inholland.codegen.bankingapp.models;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import java.util.Date;
 
 import lombok.*;
 
@@ -13,30 +13,37 @@ import lombok.*;
 @Getter
 @Setter
 public class Account {
+    public static Long DEFAULT_DAILY_LIMIT = 5_000L * 100L; // 5000.00 EUR
+    public static Long DEFAULT_ABSOLUTE_LIMIT = 0L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountId;
 
-    @Column(nullable = false, unique = true)
-    private Long accountNumber;
+    // @Column(nullable = true, unique = true)
+    // private Long accountNumber;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private AccountType accountType;
 
     @Column(nullable = false)
-    private Long storedAmountInCents;
+    @Builder.Default
+    private Long storedAmountInCents = 0L; // starting balance will always be 0
 
     @Column(nullable = false)
-    private Long dailyLimitInCents;
+    @Builder.Default
+    private Long dailyLimitInCents = DEFAULT_DAILY_LIMIT;
 
     @Column(nullable = false)
-    private Long absoluteLimitInCents;
+    @Builder.Default
+    private Long absoluteLimitInCents = DEFAULT_ABSOLUTE_LIMIT;
 
-    @Column(nullable = false)
-    private LocalDateTime creationDate;
+    @Column(nullable = false, updatable = false)
+    @Builder.Default
+    private Date creationDate = new Date();
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, updatable = false)
     private String iban;
 
     @ManyToOne
@@ -46,7 +53,7 @@ public class Account {
     @Builder.Default
     private Boolean closed = false;
 
-    public enum AccountType {
+    public static enum AccountType {
         Checking,
         Savings,
         Atm
