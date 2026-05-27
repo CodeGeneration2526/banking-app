b198279f42bdfd23898089a0c667d3fb32d5f473
piguycs <piguycs@pm.me>
2026-05-27 11:59:03 +0200

    login page with an example picocss form from their website

--- frontend/src/pages/LoginRegisterPage.vue
+++ frontend/src/pages/LoginRegisterPage.vue
@@ -1,3 +1,28 @@
 <template>
-    login/register
+    <form>
+      <fieldset>
+        <label>
+          First name
+          <input
+            name="first_name"
+            placeholder="First name"
+            autocomplete="given-name"
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            autocomplete="email"
+          />
+        </label>
+      </fieldset>
+
+      <input
+        type="submit"
+        value="Subscribe"
+      />
+    </form>
 </template>
