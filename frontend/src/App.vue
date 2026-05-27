c73362aa76c68d92cebc6b6ffaad2b1da53c36fb
piguycs <piguycs@pm.me>
2026-05-27 11:33:51 +0200

    setting up picocss and a css preprocessor (to configure picocss)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-    <h1>You did it!</h1>
-    <p>
-        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-        documentation
-    </p>
+    <header>
+        <nav>
+            <ul>
+                <li><strong>Bank</strong></li>
+            </ul>
+            <ul>
+                <li><a href="#" class="contrast">Login/Register</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>You did it!</h1>
+        <p>
+            Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+            documentation
+        </p>
+    </main>
+    <footer>
+        <small>&#169; Jonathan Mauracio<br></small>
+        <small>&#169; Kunal Dandekar<br></small>
+        <small>Licensed under the GNU <a class="secondary" target="_blank" href="https://www.gnu.org/licenses/agpl-3.0.en.html">AGPL-3.0-or-later</a> license</small>
+    </footer>
 </template>
 
 <style scoped></style>
